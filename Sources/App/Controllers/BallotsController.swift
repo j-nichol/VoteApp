ecf5b7f8e32f73c4ce96f3bc688707454fd1d9d9
Jack <j.nichol@abertay.ac.uk>
2019-01-17 16:22:58 +0000

    Attempted to add a search function to /api/ballots/search

--- Sources/App/Controllers/BallotsController.swift
+++ Sources/App/Controllers/BallotsController.swift
@@ -10,6 +10,8 @@ struct BallotsController: RouteCollection {
         ballotsRoutes.post(Ballot.self, use: createHandler)
         //retreive
         ballotsRoutes.get(use: getAllHandler)
+        //retreive specific
+        ballotsRoutes.get("search", use:searchHandler)
         //update
         ballotsRoutes.put(Ballot.parameter, use: updateHandler)
         //delete
@@ -26,6 +28,33 @@ struct BallotsController: RouteCollection {
     func getAllHandler(_ req: Request) throws -> Future<[Ballot]> {
         return Ballot.query(on: req).all()
     }
+    /*
+    func findHandler(_ req: Request) throws -> Future<Ballot> {
+        let result =  Ballot.find(try req.parameters.next(Int.self), on: req)
+        return result
+    }
+    */
+    
+    func searchHandler(_ req: Request) throws -> Future<[Ballot]> {
+        guard let searchTerm = req.query[Int.self, at: "id"] else { throw Abort(.badRequest) }
+        return Ballot.query(on: req).group(.or) { or in
+            or.filter(\.id == searchTerm)
+            }.all()
+    }
+    
+    
+    /*
+    func searchHandler(_ req: Request) throws -> Future<[Acronym]> {
+        guard let searchTerm = req
+            .query[String.self, at: "term"] else {
+                throw Abort(.badRequest)
+        }
+        return Acronym.query(on: req).group(.or) { or in
+            or.filter(\.short == searchTerm)
+            or.filter(\.long == searchTerm)
+            }.all()
+    }
+     */
     
     ///update
     //figure out how to use Type.update(on: req)
