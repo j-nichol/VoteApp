37a1ecd7f48ca8006fe528a0fd1a0a0405a92ea3
Jack <j.nichol@abertay.ac.uk>
2019-01-25 15:44:06 +0000

    End of first part of Leaf tutorial

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -2,9 +2,12 @@ import Vapor
 import Leaf
 
 struct WebsiteController: RouteCollection {
+  
   func boot(router: Router) throws {
     router.get(use: indexHandler)
+    router.get("elections", Election.parameter, use: electionHandler)
   }
+  
   func indexHandler(_ req: Request) throws -> Future<View> {
     return Election.query(on: req).all().flatMap(to: View.self) {
       elections in
@@ -13,9 +16,27 @@ struct WebsiteController: RouteCollection {
       return try req.view().render("index", context)
     }
   }
+  
+  func electionHandler(_ req: Request) throws -> Future<View> {
+    return try req.parameters.next(Election.self).flatMap(to: View.self) {
+     election in
+      return election.electionCategory.get(on: req).flatMap(to: View.self) {
+        electionCategory in
+        let context = electionContext(title: election.name, election: election, electionCategory: electionCategory)
+        return try req.view().render("election", context)
+      }
+    }
+  }
+
 }
 
 struct IndexContext: Encodable {
   let title: String
   let elections: [Election]?
+}
+
+struct electionContext: Encodable {
+  let title: String
+  let election: Election
+  let electionCategory: ElectionCategory
 }
