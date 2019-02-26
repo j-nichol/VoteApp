06b723d3494497cf0be076f329f4716bab319ec5
Jack <j.nichol@abertay.ac.uk>
2019-02-26 20:46:09 +0000

    Cors attempt 3.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,13 +17,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
   /// Register middleware
   var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-  let corsConfiguration = CORSMiddleware.Configuration(allowedOrigin: .all, allowedMethods: [.GET], allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin])
-  let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
-  middlewares.use(corsMiddleware)
+  middlewares.use(CORSMiddleware(configuration: CORSMiddleware.Configuration(allowedOrigin: .all, allowedMethods: [.GET], allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin])))
   middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
   middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
   middlewares.use(SessionsMiddleware.self) // Allows the use of sessions
-  middlewares.use(CORSMiddleware.self) //Cross-origin-resource-sharing
   services.register(middlewares)
 
   /// Configure database
