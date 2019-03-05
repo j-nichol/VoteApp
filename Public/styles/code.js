b73317a6de1f9c346056a375dfc7ec97f0ca4e7d
Jack <j.nichol@abertay.ac.uk>
2019-03-05 16:36:47 +0000

    JS Fix.

--- Public/styles/code.js
+++ Public/styles/code.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('verificationCode').select();
     document.execCommand("copy");
     toastr.options = {
-    "closeButton": true,"debug": false, "newestOnTop": false,"progressBar": true,"positionClass": "toast-top-full-width","preventDuplicates": true,"onclick": null,"showDuration": "300","hideDuration": "1000","timeOut": "5000","extendedTimeOut": "1000","showEasing": "swing","hideEasing": "linear","showMethod": "slideDown","hideMethod": "slideUp"}
-    Command: toastr["success"]("has been copied to your clipboard.", "Verification Code")
-  };
-};
+    "closeButton": true,"progressBar": true,"preventDuplicates": true};
+    toastr.success("has been copied to your clipboard.", "Verification Code");
+    });
+  });
