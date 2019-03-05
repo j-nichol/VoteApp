2670f7445ce72cc18ae9968659cca8b8cb3e8dc2
Jack <j.nichol@abertay.ac.uk>
2019-03-05 17:19:22 +0000

    Style update for notification

--- Public/styles/code.js
+++ Public/styles/code.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     console.log("called");
     $('#verificationCode').select();
     document.execCommand("copy");
-    toastr.options = {"closeButton": true,"progressBar": true,"preventDuplicates": true};
+    toastr.options = {"closeButton": true,"preventDuplicates": true};
     toastr.success("has been copied to your clipboard.", "Verification Code");
   });
 });
