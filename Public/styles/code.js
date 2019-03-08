662e31d5c78a95a7d18e785f8fa1937c107b1523
Jack <j.nichol@abertay.ac.uk>
2019-03-08 19:55:18 +0000

    Things 'n' stuff.

--- Public/styles/code.js
+++ Public/styles/code.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
                   $('#copyVerificationCode').click(function(){
-                                                   console.log("called");
                                                    $('#verificationCode').select();
                                                    document.execCommand("copy");
                                                    toastr.options = {"closeButton": true,"preventDuplicates": true};
                                                    toastr.success("has been copied to your clipboard.", "Verification Code");
+                                                   });
+                  $('#emailVerificationCode').click(function(){
+                                                   //send email
+                                                   toastr.options = {"closeButton": true,"preventDuplicates": true};
+                                                   toastr.success("has been sent to your Email Inbox.", "Verification Code");
                                                    });
                   $('.backButton').click(function(){
                                          window.history.back();
