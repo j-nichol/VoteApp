419a30b2773e20bf345835e12dc298ae703de2ce
Jack <j.nichol@abertay.ac.uk>
2019-03-11 16:13:50 +0000

    Update code.js

--- Public/styles/code.js
+++ Public/styles/code.js
@@ -22,31 +22,55 @@ $(document).ready(function(){
 //                  var spoilBallot = document.getElementById('spoilBallotHelp');
 //                  var checkVerificationCode = document.getElementById('checkVerificationCodeHelp');
                   
-                  var orangeRegion = ZingTouch.Region(orange);
-                  var userpassRegion = ZingTouch.Region(userpass);
+                  
 //                  var loginRegion = ZingTouch.Region(login);
 //                  var securelyRegion = ZingTouch.Region(securely);
 //                  var verificationCodeRegion = ZingTouch.Region(verificationCode);
 //                  var spoilBallotRegion = ZingTouch.Region(spoilBallot);
 //                  var checkVerificationCodeRegion = ZingTouch.Region(checkVerificationCode);
+                  if (orange) {
+                    var orangeRegion = ZingTouch.Region(orange);
+                    orangeRegion.bind(orange, 'swipe', function(event){
+                                      var direction = event.detail.data[0].currentDirection;
+                                      if (direction > 225 && direction < 315) {
+                                        $("#orangeHelp").slideUp("slow");
+                                        $('html').css({ "position": "relative"});
+                                        $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)"});
+                                      }
+                                      });
+                    $('#orangeHelpButton').click(function(){
+                                                 $("#orangeHelp").slideDown("slow");
+                                                 $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });
+                                                 $('html').css({ "position": "fixed"});
+                                                 });
+                    $('#orangeHelp .helpDismiss').click(function(){
+                                                        $("#orangeHelp").slideUp("slow");
+                                                        $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" });
+                                                        $('html').css({ "position": "relative"});
+                                                        });
+                  }
                   
-                  orangeRegion.bind(orange, 'swipe', function(event){
-                                    var direction = event.detail.data[0].currentDirection;
-                                    if (direction > 225 && direction < 315) {
-                                      $("#orangeHelp").slideUp("slow");
+                  if (userpass){
+                    var userpassRegion = ZingTouch.Region(userpass);
+                    userpassRegion.bind.bind(userpass, 'swipe', function(event){
+                                      var direction = event.detail.data[0].currentDirection;
+                                      if (direction > 225 && direction < 315) {
+                                      $("#userpassHelp").slideUp("slow");
                                       $('html').css({ "position": "relative"});
                                       $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)"});
-                                    }
-                                    });
-                  
-                  userpassRegion.bind.bind(userpass, 'swipe', function(event){
-                                    var direction = event.detail.data[0].currentDirection;
-                                    if (direction > 225 && direction < 315) {
-                                    $("#userpassHelp").slideUp("slow");
-                                    $('html').css({ "position": "relative"});
-                                    $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)"});
-                                    }
-                                    });
+                                      }
+                                      });
+                    $('#userpassHelpButton').click(function(){
+                                                   $("#userpassHelp").slideDown("slow");
+                                                   $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });
+                                                   $('html').css({ "position": "fixed"});
+                                                   });
+                    $('#userpassHelp .helpDismiss').click(function(){
+                                                          $("#userpassHelp").slideUp("slow");
+                                                          $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" });
+                                                          $('html').css({ "position": "relative"});
+                                                          });
+                  }
                   
 //                  userpassRegion.bind(userpass, 'swipe', function(event){ var direction = event.detail.data[0].currentDirection; if (direction > 225 && direction < 315) { $("#userpassHelp").slideUp("slow"); $('html').css({ "position": "relative"}); $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" }); } });
 //                  loginRegion.bind(login, 'swipe', function(event){ var direction = event.detail.data[0].currentDirection; if (direction > 225 && direction < 315) { $("#loginHelp").slideUp("slow"); $('html').css({ "position": "relative"}); $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" }); } });
@@ -54,18 +78,9 @@ $(document).ready(function(){
 //                  verificationCodeRegion.bind(verificationCode, 'swipe', function(event){ var direction = event.detail.data[0].currentDirection; if (direction > 225 && direction < 315) { $("#verificationCodeHelp").slideUp("slow"); $('html').css({ "position": "relative"}); $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" }); } });
 //                  spoilBallotRegion.bind(spoilBallot, 'swipe', function(event){ var direction = event.detail.data[0].currentDirection; if (direction > 225 && direction < 315) { $("#spoilBallotHelp").slideUp("slow"); $('html').css({ "position": "relative"}); $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" }); } });
 //                  checkVerificationCodeRegion.bind(checkVerificationCode, 'swipe', function(event){ var direction = event.detail.data[0].currentDirection; if (direction > 225 && direction < 315) { $("#checkVerificationCodeHelp").slideUp("slow"); $('html').css({ "position": "relative"}); $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" }); } });
+
                   
-                  $('#orangeHelpButton').click(function(){
-                                               $("#orangeHelp").slideDown("slow");
-                                               $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });
-                                               $('html').css({ "position": "fixed"});
-                                               });
                   
-                  $('#userpassHelpButton').click(function(){
-                                               $("#userpassHelp").slideDown("slow");
-                                               $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });
-                                               $('html').css({ "position": "fixed"});
-                                               });
 //                  $('#userpassHelpButton').click(function(){$("#userpassHelp").slideDown("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });$('html').css({ "position": "fixed"});});
 //                  $('#loginHelpButton').click(function(){$("#loginHelp").slideDown("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });$('html').css({ "position": "fixed"});});
 //                  $('#securelyHelpButton').click(function(){$("#securelyHelp").slideDown("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });$('html').css({ "position": "fixed"});});
@@ -73,16 +88,8 @@ $(document).ready(function(){
 //                  $('#spoilBallotHelpButton').click(function(){$("#spoilBallotHelp").slideDown("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });$('html').css({ "position": "fixed"});});
 //                  $('#checkVerificationCodeButton').click(function(){$("#checkVerificationCodeHelp").slideDown("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(31px)" });$('html').css({ "position": "fixed"});});
                   
-                  $('#orangeHelp .helpDismiss').click(function(){
-                                                      $("#orangeHelp").slideUp("slow");
-                                                      $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" });
-                                                      $('html').css({ "position": "relative"});
-                                                      });
-                  $('#userpassHelp .helpDismiss').click(function(){
-                                                      $("#userpassHelp").slideUp("slow");
-                                                      $(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" });
-                                                      $('html').css({ "position": "relative"});
-                                                      });
+                 
+                  
 //                  $('#userpassHelp .helpDismiss').click(function(){$("#userpassHelp").slideUp("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" });$('html').css({ "position": "relative"});});
 //                  $('#loginHelp .helpDismiss').click(function(){$("#loginHelp").slideUp("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" });$('html').css({ "position": "relative"});});
 //                  $('#securelyHelp .helpDismiss').click(function(){$("#securelyHelp").slideUp("slow");$(".helpWindow").css({ "-webkit-backdrop-filter": "blur(30px)" });$('html').css({ "position": "relative"});});
