a3a2cbfb82df8834c2fd6e124dd90d65bcc76a7e
Jack <j.nichol@abertay.ac.uk>
2019-03-11 05:54:38 +0000

    added slide to

--- Public/styles/code.js
+++ Public/styles/code.js
@@ -21,47 +21,12 @@ $(document).ready(function(){
                             console.log(data.distance.y);
                             });
                   
-//                  var yellowHammer = new Hammer(document.getElementById('yellowHelp'));
-//                  var userpassHammer = new Hammer(document.getElementById('userpassHelp'));
-//                  var loginHammer = new Hammer(document.getElementById('loginHelo'));
-//                  var securelyHammer = new Hammer(document.getElementById('securelyHelp'));
-//                  var verificationCodeHammer = new Hammer(document.getElementById('verificationCodeHelp'));
-//                  var spoilBallotHammer = new Hammer(document.getElementById('spoilBallotHelp'));
-//                  var checkVerificationCodeHammer = new Hammer(document.getElementById('checkVerificationCodeHelp'));
+                  $('#orangeHelpButton').click(function(){
+                                               $( "#orangeHelp" ).slideDown("slow");
+                                               });
                   
-//                  $('.helpWindow').hammer(direction: Hammer.DIRECTION_VERTICAL).bind("swipeDown", function(ev) {
-//                                                 console.log(ev);
-//                                                 });
-                  
-                  
-//                  yellowHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-//                  userpassHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-//                  loginHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-//                  securelyHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-//                  verificationCodeHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-//                  spoilBallotHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-//                  checkVerificationCodeHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-//
-//                  yellowHammer.on('swipedown', function(ev) {
-//                                  console.log(ev);
-//                                  });
-//                  userpassHammer.on('swipedown', function(ev) {
-//                                    console.log(ev);
-//                                    });
-//                  loginHammer.on('swipedown', function(ev) {
-//                                 console.log(ev);
-//                                 });
-//                  securelyHammer.on('swipedown', function(ev) {
-//                                    console.log(ev);
-//                                    });
-//                  verificationCodeHammer.on('swipedown', function(ev) {
-//                                            console.log(ev);
-//                                            });
-//                  spoilBallotHammer.on('swipedown', function(ev) {
-//                                       console.log(ev);
-//                                       });
-//                  checkVerificationCodeHammer.on('swipedown', function(ev) {
-//                                                 console.log(ev);
-//                                                 });
+                  $('#orangeHelp .helpDismiss').click(function(){
+                                                     $( "#orangeHelp" ).slideUp("slow");
+                                                      });
 });
 
