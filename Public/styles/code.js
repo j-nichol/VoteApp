68941a9dc72a1c7db143df8171610a2cf34aa200
Jack <j.nichol@abertay.ac.uk>
2019-03-11 03:47:17 +0000

    Few CSS changes, added hammer.js, added skeleton of yellowHelp to configue helpWindow

--- Public/styles/code.js
+++ Public/styles/code.js
@@ -13,5 +13,43 @@ $(document).ready(function(){
                   $('.backButton').click(function(){
                                          window.history.back();
                                          });
+                  
+                  var yellowHammer = new Hammer($('#yellowHelp'));
+                  var userpassHammer = new Hammer($('#userpassHelp'));
+                  var loginHammer = new Hammer($('#loginHelp'));
+                  var securelyHammer = new Hammer($('#securelyHelp'));
+                  var verificationCodeHammer = new Hammer($('#verificationCodeHelp'));
+                  var spoilBallotHammer = new Hammer($('#spoilBallotHelp'));
+                  var checkVerificationCodeHammer = new Hammer($('#checkVerificationCodeHelp'));
+                  
+                  yellowHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+                  userpassHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+                  loginHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+                  securelyHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+                  verificationCodeHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+                  spoilBallotHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+                  checkVerificationCodeHammer.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+                  
+                  yellowHammer.on('swipedown', function(ev) {
+                                  console.log(ev);
+                                  });
+                  userpassHammer.on('swipedown', function(ev) {
+                                    console.log(ev);
+                                    });
+                  loginHammer.on('swipedown', function(ev) {
+                                 console.log(ev);
+                                 });
+                  securelyHammer.on('swipedown', function(ev) {
+                                    console.log(ev);
+                                    });
+                  verificationCodeHammer.on('swipedown', function(ev) {
+                                            console.log(ev);
+                                            });
+                  spoilBallotHammer.on('swipedown', function(ev) {
+                                       console.log(ev);
+                                       });
+                  checkVerificationCodeHammer.on('swipedown', function(ev) {
+                                                 console.log(ev);
+                                                 });
 });
 
